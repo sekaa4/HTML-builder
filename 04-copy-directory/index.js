6e7802b48e31c570b7b21ec12df30074d3fa042f
Sergey P <s.pansevi4@gmail.com>
2022-11-01 16:29:14 +0300

    feat: complete fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const { readdir, mkdir, copyFile, rm } = require('node:fs/promises');
+const path = require('path');
 
+const absPath = path.join(__dirname, 'files');
+const absNewPath = path.join(__dirname, 'files-copy');
+
+async function createDir () {
+  try {
+    await mkdir(absNewPath, { recursive: true });
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function deleteFiles () {
+  try {
+    const files = await readdir(absNewPath);
+    for (let prop of files) {
+      await rm(path.join(absNewPath, prop));
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function copyFiles () {
+  try {
+    const files = await readdir(absPath);
+    for (let prop of files) {
+      await copyFile(path.join(absPath, prop), path.join(absNewPath, prop));
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function copyDir () {
+  await createDir();
+  await deleteFiles();
+  await copyFiles();
+}
+
+copyDir();
